5e20e2177b0d1908c66c80a73a886b2b146584b7
hamidreza-taheri <155115529+hamidreza-taheri@users.noreply.github.com>
2023-12-30 01:09:03 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,16 +2,9 @@
 using namespace std;
 
 int main() {
-   long int inputNumber, sum = 0;
-
-   cin>> inputNumber;
-
-   while (inputNumber != 0) {
-       sum += inputNumber;
-       cin>> inputNumber;
-   }
-
-   cout<< sum;
-
+    int x,y;
+    cin>>x;
+    cin>>y;
+    cout<< x + y ;
     return 0;
 }
